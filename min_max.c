3162e2cd8ffb5252d508f6c8d7d141a89343e6b2
jordanenichols <jordan.jbns@gmail.com>
2018-12-08 16:46:30 -0600

    fixed min_max.c, just need to add min and max functionality

--- min_max.c
+++ min_max.c
@@ -18,30 +18,30 @@ int main()
 
     elemptr first = (elemptr)malloc(sizeof(country_element));
     elemptr current = first;
-    first->count = 0;
+    first->count = 1;
     first->next = NULL;
     fscanf(fp, "%s", &country_name);
-    strcpy(first->country, country_name);       //Initializes first->country to the name of the first country in the file
+    strcpy(first->country, country_name);          //Initializes first->country to the name of the first country in the file
     while (fscanf(fp, "%s", &country_name) != EOF) //loops until EOF
     {
         didadd = 0;
         elemptr current = first;
         if (strcmp(country_name, current->country) == 0)
         {
-            current->count += 1;        //If the country name is the same in the file and the first element, add one to its count
+            current->count += 1;
         }
         else
         {
-            while (current->next != NULL)   //Loop until the end of the linked list
+            while (current->next != NULL) //Loop until the end of the linked list
             {
+                current = current->next;
                 if (strcmp(country_name, current->country) == 0)
                 {
-                    current->count += 1;    //If the country name is the same as another in the linked list, add one to its count
-                    didadd = 1;     //keeps track that at some point the country was found on the list
+                    current->count += 1; //If the country name is the same as another in the linked list, add one to its count
+                    didadd = 1;          //keeps track that at some point the country was found on the list
                 }
-                current = current->next;
             }
-            if (didadd == 0)       //appends new element to the end of the linked list if the country wasn't found in the linked list
+            if (didadd == 0) //appends new element to the end of the linked list if the country wasn't found in the linked list
             {
                 current->next = (elemptr)malloc(sizeof(country_element));
                 current = current->next;
@@ -51,11 +51,12 @@ int main()
             }
         }
     }
-    current = first;        //sets current to point to the first element
-    while (current->next != NULL)  
+    current = first; //sets current to point to the first element
+    while (current->next != NULL)
     {
         printf("%s %d\n", current->country, current->count);
         current = current->next;
     }
+    printf("%s %d\n", current->country, current->count);
     fclose(fp);
 }
